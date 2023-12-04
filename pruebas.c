5d09bfd029eb81e33b8298449d1b9a277816111d
rpisoner <144940000+rpisoner@users.noreply.github.com>
2023-12-04 20:54:26 +0100

    pruebas.c

--- pruebas.c
+++ pruebas.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
 #include "get_next_line.h"
 
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
 int	main(void)
 {
 	char	*buffer;
 	int		fd;
 	char	*old;
+	int		verify_read;
 	int		i;
 
 	i = 5;
 	fd = open("pruebas.txt", O_RDWR);
-	buffer = (char *)ft_calloc(BUFFER_SIZE * sizeof(char));
-	read(fd, buffer, BUFFER_SIZE);
-	old = ft_strjoin(buffer, "");
-	printf("%s\n", old);
+	buffer = (char *)malloc(BUFFER_SIZE * sizeof(char));
+	verify_read = read(fd, buffer, BUFFER_SIZE);
+	printf("%d\n", verify_read);
+	free(buffer);
+	verify_read = read(fd, buffer, BUFFER_SIZE);
+	free(buffer);
+	printf("%d\n", verify_read);
+	printf("%s\n", buffer);
 	return (0);
 }
